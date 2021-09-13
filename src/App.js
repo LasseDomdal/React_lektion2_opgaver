16c4fb9f88d4542e7dca57b1fe5e78264b3f0510
LasseDomdal <domdal.lasse@gmail.com>
2021-09-14 00:30:23 +0200

    first draft of task 4.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import HeaderClass from "./components/headerClass";
 import FooterClass from "./components/footerClass";
+import ErrorChecker from "./components/ErrorChecker";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <FooterClass footerskrift= "footerClass is here with state" />
         <Header overskrift ="header function is here with component" isLog={true} signup = "signup"/>
         <Footer footerskrift = "footer function is here with state"/>
+        <ErrorChecker/>
     </div>
   );
 }
---
+++ src/components/ErrorChecker.js
@@ -0,0 +1,16 @@
+import React, {useState} from "react";
+
+function ErrorChecker(props) {
+    const[Error, setErrors] = useState("");
+
+    return (
+        <div>
+            <h1> Error Checker</h1>
+            <input />
+            <label/>
+        </div>
+    );
+
+
+}
+export default ErrorChecker
