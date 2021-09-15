0279192df5bed57407c046570062673c4a5329cc
LasseDomdal <domdal.lasse@gmail.com>
2021-09-15 21:38:50 +0200

    Finished task 4, with a input and input validator.

--- src/components/ErrorChecker.js
+++ src/components/ErrorChecker.js
@@ -1,16 +1,32 @@
 import React, {useState} from "react";
 
-function ErrorChecker(props) {
-    const[Error, setErrors] = useState("");
+function ErrorChecker() {
+    const [Input, setInput] = useState("");
+    const [Error, setErrors] = useState([]);
 
-    return (
-        <div>
-            <h1> Error Checker</h1>
-            <input />
-            <label/>
-        </div>
-    );
+    const validateInput = (value) => {
+        if (value.length > 7) {
+            return ["Value too long"]
+        }
+        return []
+    }
 
+    const handleOnChange = (event) => {
+        const {name, value} = event.target
+        if (name === "Input") {
+            setInput(value)
+            setErrors(validateInput(value))
+        }
+    }
+        return (
+            <div>
+                {/*<form onSubmit={handleSubmit}>*/}
+                <h1> Error Checker</h1>
+                <input name="Input" value={Input.value} onChange={handleOnChange}/>
+                <div className="form-element-error">{Error.map((error, index) => <p key={index}>{error}</p>)}</div>
+                {/*</form>*/}
+            </div>
+        );
 
 }
 export default ErrorChecker
